dec00f378db91d6265a93a01a3ff48abf527c9b8
Sergey Tolmachev <tolsi.ru@gmail.com>
2020-11-21 18:45:35 +0300

    bfs until timer
    silver arena!

--- src/Bot.kt
+++ src/Bot.kt
@@ -96,28 +96,63 @@ object RealGame : Simulation() {
         println(m.asText)
         return null
     }
+
+    fun makeTestMove(): Move {
+        return botStep(
+            GameState(
+                1,
+                Player(3, 0, 0, 0, 0),
+                Player(3, 0, 0, 0, 0),
+                listOf(
+                    Action(70, ActionType.BREW, -2, -2, 0, -2, 15, -1, -1, false, false),
+                    Action(72, ActionType.BREW, 0, -2, -2, -2, 19, -1, -1, false, false),
+                    Action(58, ActionType.BREW, 0, -3, 0, -2, 14, -1, -1, false, false),
+                    Action(50, ActionType.BREW, -2, 0, 0, -2, 10, -1, -1, false, false),
+                    Action(67, ActionType.BREW, 0, -2, -1, -1, 12, -1, -1, false, false),
+                    Action(78, ActionType.CAST, 2, 0, 0, 0, 0, -1, -1, true, false),
+                    Action(79, ActionType.CAST, -1, 1, 0, 0, 0, -1, -1, true, false),
+                    Action(80, ActionType.CAST, 0, -1, 1, 0, 0, -1, -1, true, false),
+                    Action(81, ActionType.CAST, 0, 0, -1, 1, 0, -1, -1, true, false),
+                    Action(82, ActionType.OPPONENT_CAST, 2, 0, 0, 0, 0, -1, -1, true, false),
+                    Action(83, ActionType.OPPONENT_CAST, -1, 1, 0, 0, 0, -1, -1, true, false),
+                    Action(84, ActionType.OPPONENT_CAST, 0, -1, 1, 0, 0, -1, -1, true, false),
+                    Action(85, ActionType.OPPONENT_CAST, 0, 0, -1, 1, 0, -1, -1, true, false)
+                )
+            ), System.currentTimeMillis()
+        )
+    }
 }
 //endregion
 
 object FastSimulation : Simulation() {
-    // TODO: учитывать предыдущие ходы, не больше двух REST
     fun allowedMoves(currentState: GameState, prevMoves: List<Move>): List<Move> {
+        val lastOpt = prevMoves.lastOrNull()
         val moves0 = currentState.actions.flatMap { a ->
-            val times = a.canBePlayedTimes(currentState.me)
-            if (times == 1) {
-                if (a.actionType == ActionType.LEARN) {
-                    listOf(Learn(a))
-                } else {
-                    listOf(Play(a))
+            val untilBrew = lastOpt?.let {
+                when (it) {
+                    is Play -> it.action.actionType == ActionType.BREW
+                    else -> false
                 }
-            } else if (times > 1) {
-                (1..times).map { Play(a, it) }
-            } else {
+            } ?: false
+            if (untilBrew || prevMoves.size > 7) {
                 emptyList()
+            } else {
+                val times = a.canBePlayedTimes(currentState.me)
+                if (times == 1) {
+                    if (a.actionType == ActionType.LEARN) {
+                        listOf(Learn(a))
+                    } else {
+                        listOf(Play(a))
+                    }
+                } else if (times > 1) {
+                    (1..times).map { Play(a, it) }
+                } else {
+                    emptyList()
+                }
             }
         }
 
-        val moves1 = if (prevMoves.lastOrNull()?.let { it == Rest } ?: false) {
+        val moves1 = if (lastOpt?.let { it == Rest } ?: false) {
             moves0
         } else moves0.plus(Rest)
 
@@ -150,40 +185,88 @@ object FastSimulation : Simulation() {
                     ), actions = currentState.actions.minus(m.action)
                 )
                 if (m.action.actionType == ActionType.CAST) {
-                    currentStateAfterAction.copy(actions = currentState.actions.plus(m.action.copy(castable = false)))
+                    currentStateAfterAction.copy(actions = currentStateAfterAction.actions.plus(m.action.copy(castable = false)))
                 } else currentStateAfterAction
             }
             else -> TODO()
         }
     }
 
-    private fun makeAllAllowedMoves(gameState: GameState, moves: List<Move> = ArrayList()): List<Pair<List<Move>, GameState>> {
-        // TODO random take 5!
-        return allowedMoves(gameState, moves).shuffled().take(5).map { moves + it to makeMove(gameState, it)!! }
+    private fun makeAllAllowedMoves(
+        gameState: GameState,
+        moves: List<Move> = ArrayList()
+    ): List<Pair<List<Move>, GameState>> {
+        return allowedMoves(gameState, moves).map { moves + it to makeMove(gameState, it)!! }
     }
 
-    fun findBestMove(gameState: GameState, depth: Int): Pair<List<Move>, GameState> {
-        val moves =  (0..depth).fold(emptyList<Pair<List<Move>, GameState>>() to listOf(emptyList<Move>() to gameState)) {
-                current, i ->
-            val nextMoves = current.second.flatMap { makeAllAllowedMoves(it.second, it.first) }
-            (if (i > 0) { current.first.plus(current.second) } else current.first) to nextMoves
-        }.first
-        return moves.maxBy { (it.second.me.score - gameState.me.score) / it.first.size }!!
+    var bestFound: Pair<List<Move>, GameState>? = null
+    var bestValue: Int = -100000
+    var seen: Int = 0
+
+    fun resetState() {
+        bestFound = null
+        bestValue = -100000
+        seen = 0
+    }
+
+    fun findBestMove(
+        gameState: GameState,
+        stepStartMillis: Long,
+        maxMillis: Long = 50
+    ): Pair<List<Move>, GameState> {
+        val q = LinkedList<Pair<List<Move>, GameState>>()
+        q.push(Pair(emptyList(), gameState))
+
+        fun checkTime(stepStart: Long): Boolean {
+            val stepEnd = System.currentTimeMillis()
+            val stepTime = stepEnd - stepStart
+            return stepEnd + stepTime > stepStartMillis + maxMillis
+        }
+
+        loop@ while (true) {
+            val stepStart = System.currentTimeMillis()
+
+            for (i in 0 until 50) {
+                val current = q.poll()
+
+                if (current != null) {
+                    val nextStates = makeAllAllowedMoves(current.second, current.first)
+                    q.addAll(nextStates)
+
+                    val currentScore =
+                        (current.second.me.totalScore - gameState.me.score) * 10000 - Math.max(current.first.size, 1)
+                    if (currentScore > bestValue) {
+                        bestValue = currentScore
+                        bestFound = current
+                    }
+                }
+            }
+            seen += 50
+
+            if (q.isEmpty() || checkTime(stepStart)) break@loop
+        }
+        return bestFound!!
     }
 }
 
-fun botStep(currentState: GameState): Move {
+fun botStep(currentState: GameState, stepStartMillis: Long): Move {
     // TODO small depth!
-    val bestAction = FastSimulation.findBestMove(currentState, 5)
+    val bestAction = FastSimulation.findBestMove(currentState, stepStartMillis, 30)
+    return bestAction.first.firstOrNull() ?: Rest
+}
 
-    return bestAction.first.firstOrNull()?.let {
-        System.err.println("I will play move $it");
-        it
-    } ?: Rest
+fun warmUp(n: Int) {
+    for (i in 0..n) {
+        val m = RealGame.makeTestMove()
+        m.asText
+    }
 }
 
 fun main(args: Array<String>) {
     val input = Scanner(System.`in`)
+
+    warmUp(10)
+    FastSimulation.resetState()
 
     var step = 0
     // game loop
@@ -191,6 +274,7 @@ fun main(args: Array<String>) {
         step++;
         //region Read objects
         val actionCount = input.nextInt() // the number of spells and recipes in play
+        val stepStart = System.currentTimeMillis()
         val actions: List<Action> = sequence {
             for (i in 0 until actionCount) {
                 val actionId = input.nextInt() // the unique ID of this spell or recipe
@@ -234,6 +318,12 @@ fun main(args: Array<String>) {
         // To debug: System.err.println("Debug messages...");
 
         // in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
-        RealGame.makeMove(currentState, botStep(currentState))
+        RealGame.makeMove(currentState, botStep(currentState, stepStart))
+
+        System.gc()
+
+        val stepTime = System.currentTimeMillis() - stepStart
+        System.err.println("Step time: $stepTime, seen: ${FastSimulation.seen}")
+        FastSimulation.resetState()
     }
 }
--- src/StartBot.kt
+++ src/StartBot.kt
@@ -1,26 +1,5 @@
 fun main() {
-    println(
-        botStep(
-            GameState(
-                1,
-                Player(3, 0, 0, 0, 0),
-                Player(3, 0, 0, 0, 0),
-                listOf(
-                    Action(70, ActionType.BREW, -2, -2, 0, -2, 15, -1, -1, false, false),
-                    Action(72, ActionType.BREW, 0, -2, -2, -2, 19, -1, -1, false, false),
-                    Action(58, ActionType.BREW, 0, -3, 0, -2, 14, -1, -1, false, false),
-                    Action(50, ActionType.BREW, -2, 0, 0, -2, 10, -1, -1, false, false),
-                    Action(67, ActionType.BREW, 0, -2, -1, -1, 12, -1, -1, false, false),
-                    Action(78, ActionType.CAST, 2, 0, 0, 0, 0, -1, -1, true, false),
-                    Action(79, ActionType.CAST, -1, 1, 0, 0, 0, -1, -1, true, false),
-                    Action(80, ActionType.CAST, 0, -1, 1, 0, 0, -1, -1, true, false),
-                    Action(81, ActionType.CAST, 0, 0, -1, 1, 0, -1, -1, true, false),
-                    Action(82, ActionType.OPPONENT_CAST, 2, 0, 0, 0, 0, -1, -1, true, false),
-                    Action(83, ActionType.OPPONENT_CAST, -1, 1, 0, 0, 0, -1, -1, true, false),
-                    Action(84, ActionType.OPPONENT_CAST, 0, -1, 1, 0, 0, -1, -1, true, false),
-                    Action(85, ActionType.OPPONENT_CAST, 0, 0, -1, 1, 0, -1, -1, true, false)
-                )
-            )
-        )
-    )
+    for (i in 0..100000) {
+        println(RealGame.makeTestMove())
+    }
 }
